35d2be294f8c86ec1d1d91eb2beb715170be64fa
phil <phil@a721bac0-29f8-11de-897e-ef2ff8275b85>
2003-06-15 04:17:35 +0000

    Contribution to a set of tasks which will virtually compute a flux over a network of processors

--- Projects/Flux/TContribution.cpp
+++ Projects/Flux/TContribution.cpp
@@ -6,40 +6,50 @@
 
 bool TContribution::operator == (const TContribution & contr)
 {
-    if(!(fDestinationMesh == contr.fDestinationMesh)) return false;
-    if(fFrom.size() != contr.fFrom.size()) return false;
-    if(fIsEmpty != contr.fIsEmpty) return false;
-    if(fLocalIndices.size() != contr.fLocalIndices.size()) return false;
-    if(fNContributions != contr.fNContributions) return false;
-    if(fTo.size() != contr.fTo.size()) return false;
-	return true;
-
+  if(!(fDestinationMesh == contr.fDestinationMesh)) return false;
+  if(fFrom.size() != contr.fFrom.size()) return false;
+  if(fLocalIndices.size() != contr.fLocalIndices.size()) return false;
+  if(fNContributions != contr.fNContributions) return false;
+  if(fTo.size() != contr.fTo.size()) return false;
+  return true;
+  
 }
 TContribution & TContribution::operator=(const TContribution  & contr)
 {
-    fDestinationMesh = contr.fDestinationMesh;
-    fFrom = contr.fFrom;
-    fIsEmpty = contr.fIsEmpty;
-    fLocalIndices = contr.fLocalIndices;
-    fNContributions = contr.fNContributions;
-    fTo = contr.fTo;
-	return *this;
+  fDestinationMesh = contr.fDestinationMesh;
+  fFrom = contr.fFrom;
+  fLocalIndices = contr.fLocalIndices;
+  fNContributions = contr.fNContributions;
+  fTo = contr.fTo;
+  return *this;
 }
 TContribution::TContribution(){
-	fIsEmpty = 0;
 	fNContributions = 0;
 }
 int TContribution::IsEmpty(){
-	return fIsEmpty;
+	return fNContributions == 0;
 }
 vector<int> & TContribution::DestinationVector(int index){
+  if(index < (int) fLocalIndices.size()) {
     return fLocalIndices[index];
+  } else {
+    cout << "TContribution::DestinationVector index out of range index " << index << endl;
+    return fLocalIndices[0];
+  }
 }
 
 
-void TContribution::InitializeRandom(int i){
-	srand(i);
-	int rs = rand();
-	if ((rs/10) > 2) fIsEmpty = 1;
-	
+void TContribution::InitializeRandom(){
+  int rs = rand();
+  fNContributions = rs%5;
+  fLocalIndices.resize(fNContributions);
+  int ic;
+  for(ic=0; ic<fNContributions; ic++) {
+    fLocalIndices[ic].resize(rand() %20);
+    int nl = fLocalIndices[ic].size();
+    int ind;
+    for(ind=0; ind<nl; ind++) {
+      fLocalIndices[ic][ind] = ind+10;
+    }
+  }
 }

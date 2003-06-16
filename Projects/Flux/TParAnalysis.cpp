5560a80f93e46406931a4c6da62f77a2c8b3e0c7
phil <phil@a721bac0-29f8-11de-897e-ef2ff8275b85>
2003-06-16 02:02:06 +0000

    Late sunday editing to put the code toghether. I believe we are getting close to a code which can be checked

--- Projects/Flux/TParAnalysis.cpp
+++ Projects/Flux/TParAnalysis.cpp
@@ -16,31 +16,31 @@ void TParAnalysis::SetupEnvironment(){
   int ip;
   // message #1.3 to rhs:TParRhs
   vector<TParRhs *> RhsVec(npartitions);
-  vector<OOPObjectId> RhsId(npartitions);
+  fRhsId.resize(npartitions);
   for(ip=0; ip<npartitions; ip++) RhsVec[ip] = new TParRhs();
 
   // message #1.4 to state:TParState
   vector<TParState *> StateVec(npartitions);
-  vector<OOPObjectId> StateId(npartitions);
+  fStateId.resize(npartitions);
   for(ip=0; ip<npartitions; ip++) StateVec[ip] = new TParState();
 
   // message #1.5 to mesh:TParMesh
   vector<TParMesh *> MeshVec(npartitions);
-  vector<OOPObjectId> MeshId(npartitions);
+  fMeshId.resize(npartitions);
   for(ip=0; ip<npartitions; ip++) MeshVec[ip] = new TParMesh();
 
   int i;
   // message #1.7 to ver:OOPDataVersion
   for (i = 0; i < npartitions; i++) {
     // message #1.8.1 to DM:OOPDataManager
-    RhsId[i] = DM->SubmitObject(RhsVec[i], 1);
+    fRhsId[i] = DM->SubmitObject(RhsVec[i], 1);
 
     // message #1.8.3 to DM:OOPDataManager
-    StateId[i] = DM->SubmitObject(StateVec[i], 1);
+    fStateId[i] = DM->SubmitObject(StateVec[i], 1);
 
     // message #1.8.5 to DM:OOPDataManager
-    MeshId[i] = DM->SubmitObject(MeshVec[i], 1);
-    table->SetMeshId(i,MeshId[i]);
+    fMeshId[i] = DM->SubmitObject(MeshVec[i], 1);
+    table->SetMeshId(i,fMeshId[i]);
   }
   // message #1.6 to ver:OOPDataVersion
   OOPDataVersion ver;
@@ -56,15 +56,15 @@ void TParAnalysis::SetupEnvironment(){
 
   st = EVersionAccess;
   for(ip=0; ip<npartitions; ip++) {
-    AddDependentData(MeshId[ip],st,ver);
+    AddDependentData(fMeshId[ip],st,ver);
   }
 
   for(ip=0; ip<npartitions; ip++) {
-    AddDependentData(StateId[ip],st,ver);
+    AddDependentData(fStateId[ip],st,ver);
   }
 
   for(ip=0; ip<npartitions; ip++) {
-    AddDependentData(RhsId[ip],st,ver);
+    AddDependentData(fRhsId[ip],st,ver);
   }
 
 }
@@ -105,20 +105,12 @@ void TParAnalysis::CreateParCompute() {
   TParCompute * pc = new TParCompute(GetProcID(),fNumPartitions);
 
   OOPDataVersion ver;
+  pc->SetMeshId(fMeshId,ver);
+  pc->SetRhsId(fRhsId,taskver);
+  pc->SetStateId(fStateId,taskver);
+  pc->SetPartitionRelationId(tableid,ver);
   OOPMDataState st = EReadAccess;
-  pc->AddDependentData(tableid,st,ver);
-  // set mesh dependency
-  count = 0;
-  dep = fDataDepend.begin();
-  while(count < fNumPartitions) {
-    pc->AddDependentData((*dep).ObjPtr()->Id(),st,(*dep).ObjPtr()->Version());
-  }
-  // set the dependency on the state and rhs objects
-  count =0;
-  while(count < 2*fNumPartitions) {
-    pc->AddDependentData((*dep).ObjPtr()->Id(),st,taskver);
-  }
-
+  //  pc->AddDependentData(tableid,st,ver);
 
   //	pc->SetPartitionRelationId(tableId , ver);
   // message #1.9 to pc:TParCompute
@@ -143,3 +135,39 @@ void TParAnalysis::CreateParCompute() {
   }
 }
 
+void TParAnalysis::SetAppropriateVersions() {
+  OOPDataVersion ver;
+  //  OOPMDataState st = EReadAccess;
+  deque<OOPMDataDepend>::iterator dep;
+  dep = fDataDepend.begin();
+  while(dep != fDataDepend.end()) {
+    OOPDataVersion solver = (*dep).ObjPtr()->Version();
+    AdaptSolutionVersion(solver);
+    (*dep).ObjPtr()->SetVersion(solver,Id());
+  }
+}
+
+void TParAnalysis::AdaptSolutionVersion(OOPDataVersion &version) {
+
+  int depth = fTaskVersion.GetNLevels();
+  int versdepth = version.GetNLevels();
+  int d;
+  for(d=versdepth; d<depth; d++) {
+    int taskver = fTaskVersion.GetLevelVersion(d);
+    if(taskver != -1) {
+      version.IncrementLevel(taskver+1);
+      version.SetLevelVersion(d,taskver);
+    } else {
+      version.IncrementLevel(1);
+      version.SetLevelVersion(d,1);
+    }
+  }
+}
+
+OOPMReturnType TParAnalysis::Execute() {
+
+  if(fRelationTable.IsZero()) SetupEnvironment();
+  else if(fTaskVersion.GetNLevels() <= 1) CreateParCompute();
+  else SetAppropriateVersions();
+  return ESuccess;
+}

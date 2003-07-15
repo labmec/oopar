79f07f80ec3d9b453f0157b95bc574df1bd532d2
longhin <longhin@a721bac0-29f8-11de-897e-ef2ff8275b85>
2003-07-15 19:22:36 +0000

    MPI communication first inclusion

---
+++ Sources/oopmpicomm.cpp
@@ -0,0 +1,165 @@
+//
+// Autor: Fábio Amaral de Castro, RA: 991722
+//
+// E-mail: facastro99@yahoo.com
+//
+// Arquivo: com_mpi.c
+//
+// Classe: TMpiComManager
+//
+// Descr.: Classe para comunicacao utilizando MPI
+//
+// Versao: 12 / 2002
+//
+
+#include "mpi.h"
+#include "communic.h"
+#include <stdio>
+//#include <stdlib.h>
+#include <string>
+#include "oopmpicomm.h"
+#include "ooptaskman.h"
+
+class OOPMPIReceiveStorage;
+class OOPMPICommManager;
+
+using namespace std;
+
+extern TTaskManager *TM;
+
+OOPMPICommManager::OOPMPICommManager(int argc, char **argv)
+{
+  f_buffer = (PTSendStorageMpi *) NULL;
+  f_myself = -1;
+  int f_num_proc = 0;
+  f_argc = argc;
+  f_argv = argv;
+  //f_proc = (int *) NULL; 
+  //MPI_Init(&argc,&argv);
+}
+
+OOPMPICommManager::~OOPMPICommManager()
+{
+  for ( int i = 0; i < f_num_proc; i++ )
+    if ( f_buffer[i] )
+      delete( f_buffer[i] );
+  delete[] f_buffer;
+  MPI_Finalize();
+}
+
+ //Diferente de PVM, o argumento Destino em mpi nao eh o endereco absoluto do 
+  //destino, mas o relativo. ou seja, o comando MPI_INIT inicializa os
+  //processos e para cada um destina um numero inteiro diferente, a partir de 0,
+  //que é o processo que inicializa os demais.
+  //Para inicializar, necessita de variaveis int argc e char **argv.
+int OOPMPICommManager::Initialize(char *process_name, int num_of_process)
+{
+  //cout << "process_name:" << argv <<endl;;
+  MPI_Init(&f_argc,&f_argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &f_num_proc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &f_myself);
+
+  f_buffer = new(PTSendStorageMpi[ f_num_proc]);
+  if ( f_buffer == NULL )  Finish( "Initialize <Error allocating sending buffers>" );
+  for ( int i = 0; i < f_num_proc; i++ ){
+    if ( i == f_myself ){
+      f_buffer[i] = (TSendStorageMpi *)NULL;
+    }
+    else   {
+      f_buffer[i] = new TSendStorageMpi(i);  // inicializador nao-padrao
+      //if ( f_buffer[i] == NULL ) Finish( "Initialize <Error making sendings buffers>");
+    }
+  }
+
+  
+  if(f_myself==0) return f_num_proc;
+  else return 0;
+}
+  
+int OOPMPICommManager::SendTaskVrt(TTask *pTask)
+{
+  int process_id = pTask->GetProcID(); //processo onde ptask deve ser executada
+  // Se "process_id" nao for valido.
+  if ( process_id >= f_num_proc )
+    Finish( "SendObject <process ID out of range>" );
+  // Se estiver tentando enviar para mim mesmo.
+  if ( process_id == f_myself )
+    Finish( "SendObject <I cannot send to myself>" );
+
+ // Se 'process_id' < 0, faz um multi cast.
+ //se process_id < 0, first = 0. se > 0, first = process_id.
+  int first = ( process_id < 0 ? 0 : process_id );
+  //se process_id < 0, last = numero de processos. se > 0, last = process_id + 1.
+  int last  = ( process_id < 0 ? f_num_proc : process_id+1 ); //
+
+  char has_an_object = 1;
+  for ( int i = first; i < last; i++ )
+    if ( i != f_myself )
+      {
+    	// Empacota o objeto no buffer destino.
+    	pTask->SetProcID( i );      //muda destino para processador i          
+    	TSendStorageMpi *buf = f_buffer[i]; 
+    	//buf->Activate();     //  soh no pvm
+    	buf->PkByte( &has_an_object ); //empacota primeiro valor = int = 1
+    	pTask->Pack( buf );
+      }
+  
+  // Restaura o ID do processador destino na 'task'.
+  pTask->SetProcID( process_id );  
+  
+  return 1;
+};
+
+int OOPMPICommManager::ReceiveMessages(){
+  TReceiveStorageMpi msg;
+  int ret = msg.Receive();
+  // Se nao tiver mensagem, retorna.
+  if ( ret <= 0 ) return( ret );
+  ProcessMessage(msg);
+  return 1;
+};
+
+int OOPMPICommManager::ReceiveBlocking(){
+  TReceiveStorageMpi msg;
+  int ret = msg.ReceiveBlocking();
+  // se houver erro, Kill
+  if ( ret <= 0 ) Finish("ReceiveBlocking <receive error>");
+  ProcessMessage(msg);
+  return 1;
+};
+
+
+int OOPMPICommManager::SendMessages(){
+  TSendStorageMpi *p;
+  char end_of_message = 0;
+  int  length = 0;
+  for ( int i = 0; i < f_num_proc; i++ ) {
+    p = f_buffer[i];
+    if ( p != NULL  &&  p->Length() > 0 ){
+      p->PkByte( &end_of_message ); //coloca int = 0 no final da mensagem
+      if ( p->Send(1) < 1 )//msg_id=1, target=TSendStorage::f_target_tid  
+	Finish ("SendMessages <send error>" );
+    }
+  } 
+  return( 1 );
+};
+
+int OOPMPICommManager::ProcessMessage( OOPMPIReceiveStorage &msg ){
+  Trace("Recebendo uma mensagem do processador ");
+  Trace( FindID(msg.GetSender())<<"\n");
+  int count;
+  char has_more_objects = 0;
+  msg.UpkByte( &has_more_objects );
+  for ( count = 0; has_more_objects; count++ )  {
+    TSaveable *obj = Restore( &msg );
+    
+    if ( obj == NULL )
+      Finish( "ReceiveMessages <Erro em Restore() do objeto>.\n" );
+    Trace( "  ClassID do objeto recebido: " );
+    Trace( obj->GetClassID() << ".\n" );
+    
+    TM->Submit( (TTask *)obj );
+    msg.UpkByte( &has_more_objects );
+  }
+}
+
--- Sources/oopmpistorage.cpp
+++ Sources/oopmpistorage.cpp
@@ -2,7 +2,7 @@
 //
 // Autor:   Fabio Amaral Castro,  RA: 991722
 //
-// Arquivo: storage_mpi.c
+// Arquivo: str_mpi.c
 //
 // Classe:  TSendStorageMpi,
 //          TReceiveStorageMpi.
@@ -10,79 +10,257 @@
 // Descr.: 
 //          TSendStorageMpi:
 //            Classe derivada de TSendStorage que implementa o buffer
-//            usando PVM3.3.7 para a comunicacao entre processos.
+//            usando MPI para a comunicacao entre processos.
 //
 //          TReceiveStorageMpi:
 //            Classe derivada de TReceiveStorage que implementa o buffer
-//            usando PVM3.3.7 para a comunicacao entre processos.
+//            usando MPI para a comunicacao entre processos.
 //
 // Versao:  01 / 03.
 //
-#include "oopstorage.h"
-#include "oopmpistorage.h"
+#include "storage.h"
+#include "str_mpi.h"
+#include "mpi.h"
+
 
 //TSend
 
-int OOPMPISendStorage::PkByte(char *p, int n=1){
-  maxbuff=position+n;
-  MPI_Pack(p,n,MPI_CHAR,buffr,maxbuff,&position,MPI_COMM_WORLD);
-  position=maxbuff;
+OOPMPISendStorage::OOPMPISendStorage(int f_target){
+  f_target_tid = f_target;
+  ResetBuffer();
+}
+  
+  // Metodos para EMPACOTAR os dados a serem enviados.
+  //  p : Ponteiro para o buffer que contem os dados a serem empacotados.
+  //  n : Numero de elementos no buffer (default: um unico dado).
+  //
+int OOPMPISendStorage::PkByte(char *p, int n){
+  if(f_position+n*(sizeof(char))>f_size) ExpandBuffer(n*(sizeof(char))+1000);
+  MPI_Pack(p,n,MPI_CHAR,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkInt(int *p, int n){
+  if(f_position+n*(sizeof(int))>f_size) ExpandBuffer(n*(sizeof(int))+1000);
+  MPI_Pack(p,n,MPI_INT,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkShort(short *p, int n){
+  if(f_position+n*(sizeof(short))>f_size) ExpandBuffer(n*(sizeof(short))+1000);
+  MPI_Pack(p,n,MPI_SHORT,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkLong(long *p, int n){
+  if(f_position+n*(sizeof(long))>f_size) ExpandBuffer(n*(sizeof(long))+1000);
+  MPI_Pack(p,n,MPI_LONG,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+ 
+int OOPMPISendStorage::PkUint(u_int *p, int n){
+  if(f_position+n*(sizeof(u_int))>f_size) ExpandBuffer(n*(sizeof(u_int))+1000);
+  MPI_Pack(p,n,MPI_UNSIGNED,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkUshort(u_short *p, int n){
+  if(f_position+n*(sizeof(u_short))>f_size) ExpandBuffer(n*(sizeof(u_short))+1000);
+  MPI_Pack(p,n,MPI_UNSIGNED_SHORT,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkUlong(u_long *p, int n){
+  if(f_position+n*(sizeof(u_long))>f_size) ExpandBuffer(n*(sizeof(u_long))+1000);
+  MPI_Pack(p,n,MPI_UNSIGNED_LONG,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkFloat(float *p, int n){
+  if(f_position+n*(sizeof(float))>f_size) ExpandBuffer(n*(sizeof(float))+1000);
+  MPI_Pack(p,n,MPI_FLOAT,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkDouble(double *p, int n){
+  if(f_position+n*(sizeof(double))>f_size) ExpandBuffer(n*(sizeof(double))+1000);
+  MPI_Pack(p,n,MPI_DOUBLE,f_buffr,f_size,&f_position,MPI_COMM_WORLD);
+  return 1;
+}
+
+int OOPMPISendStorage::PkStr(char *p){
+  //em PkStr, conta-se o número de caracteres na string para depois empacotá-la
+  int n=0;
+  for(;p[n] != '\0';n++);
+  if(f_position+n*(sizeof(char))>f_size) ExpandBuffer(n*(sizeof(char))+1000);
+  int maxbuff=f_position+n*(sizeof(char));
+  MPI_Pack(p,n,MPI_CHAR,f_buffr,maxbuff,&f_position,MPI_COMM_WORLD);
   return 0;
 }
 
-int OOPMPISendStorage::Send(int msg_id, int tid){
-  MPI_Send(&position,1,MPI_INT,tid,msg_id,MPI_COMM_WORLD);
-  MPI_Send(buffr,position,MPI_PACKED,tid,msg_id,MPI_COMM_WORLD);
-  return 0;
+void OOPMPISendStorage::ExpandBuffer(int more_dimension){ 
+  if(more_dimension<0) Finish("ExpandBuffer <Cannot accept negative number as an argument>");
+  f_size = f_size + more_dimension;
+  const char *prov_buffer = f_buffr;
+  f_buffr = new(char[sizeof(int)]);
+  for(int i=0;i<f_position;i++) f_buffr[i]=prov_buffer[i];
+  return;
 }
 
-int OOPMPIReceiveStorage::Receive(int msg_id, int tid){
-  MPI_Status status;	       
-  //como saber tamanho do dado a receber???
-  MPI_Recv(&size,1 MPI_INT,tid,msg_id,MPI_COMM_WORLD,&status);
-  buffr = new(char[size]);
-  MPI_Recv(buffr,size,MPI_PACKED,tid,msg_id,MPI_COMM_WORLD,&status);
-  return 0;
+int OOPMPISendStorage::ResetBuffer(){
+  f_position=sizeof(int);
+  f_size=sizeof(int);
+  f_buffr = new(char[sizeof(int)]);
+  return 1;
 }
 
-int OOPMPIReceiveStorage::UpkByte(char *p, int n=1){
-  MPI_Unpack(buffr,size,&position,p,n,MPI_CHAR,MPI_COMM_WORLD);
-  position=position+n;
-  return 0;
-}
 
-//TReceive
-
-OOPSaveable *OOPMPIReceiveStorage::Restore()
-{
-  long class_id;
-	//Assumindo 1 como número de elementos a serem lidos.
-  UpkLong(&class_id, 1);
-  if ( !class_id )
-    return( 0 );
-
-  map<long,void*>::iterator i;
-  i = gFuncTree.find(class_id);
-  //i = gFuncTree.find(class_id);
-  if (i==gFuncTree.end())
-    {
-      char str[20];
-      sprintf( str, "%d", (int)class_id );
-      Error( 1, "Restore <class_id %s not recognized>\n", str );
+int OOPMPISendStorage::Send(int msg_id){
+  //MPI_Send(&f_position,1,MPI_INT,f_target_tid,msg_id,MPI_COMM_WORLD);
+  int size_position=0,max_buffr_size = 1000000, ret;
+  //empacota extensao no inicio do pacote
+  MPI_Pack(&f_position,1,MPI_INT,f_buffr,f_position,&size_position,MPI_COMM_WORLD);
+  //envia 1000000 bytes de cada vez
+  for(int i=0;i<f_position;i = i+max_buffr_size){
+    char *send_buffr = f_buffr + i;
+    int block_size = max_buffr_size;
+    if(f_position<i+max_buffr_size) {
+      block_size = f_position-i;
     }
-
-  TRestFunction f;
-  //void *c = i;//gFuncTree.contents(i);
-  f = (TRestFunction) gFuncTree[class_id];//i;//c;
-
-  return f(this);
-
+    ret = MPI_Send(send_buffr,block_size,MPI_PACKED,f_target_tid,msg_id,MPI_COMM_WORLD);
+  }
+  if (ret>0) ResetBuffer();
+  return ret;
 }
 
+//       TReceiveStorageMpi
 
-void OOPMPIReceiveStorage::AddClassRestore(long classid,TRestFunction f)
-{
-  gFuncTree[classid]= (void *) f;
+
+int OOPMPIReceiveStorage::Receive(){    //nonblocking!!!!
+  MPI_Status status;
+  MPI_Request request;
+  int max_buffr_size=1000000,size_position=0,request_flag;
+  char *receive_buffer;
+  receive_buffer = new(char[max_buffr_size]);
+  //recebe (nonblocking) primeros 10^6 bytes
+  MPI_Irecv(receive_buffer,max_buffr_size,MPI_PACKED,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&request);
+  MPI_Test(&request,&request_flag,&status);
+  if(request_flag){
+    //desempacota dimensao do pacote completo
+    MPI_Unpack(receive_buffer,max_buffr_size,&size_position,&f_size,1,MPI_INT,MPI_COMM_WORLD);
+    f_buffr = new(char[f_size]);
+    f_sender_tid = status.MPI_SOURCE;
+    f_msg_id = status.MPI_TAG;
+    f_position=sizeof(int);
+    //recebe demais blocos de 10^6 bytes
+    int receive_position;
+    for(receive_position=0;receive_position<max_buffr_size;receive_position++) 
+      if(receive_position<f_size) f_buffr[receive_position] = receive_buffer[receive_position];
+    for(int i=max_buffr_size;i<f_size;i = i+max_buffr_size){
+      MPI_Recv(receive_buffer,max_buffr_size,MPI_PACKED,f_sender_tid,f_msg_id,MPI_COMM_WORLD,&status);
+      for(int j=0;j<max_buffr_size;j++,receive_position++) 
+	if(receive_position<f_size) f_buffr[receive_position] = receive_buffer[receive_position];
+    }
+    
+    return 1;
+  }
+  else{
+    MPI_Cancel(&request);
+    return 0;
+  }
+}
+
+int OOPMPIReceiveStorage::ReceiveBlocking(){
+  MPI_Status status;	       
+  int max_buffr_size=1000000,size_position=0;
+  char *receive_buffer;
+  receive_buffer = new(char[max_buffr_size]);
+  //recebe primeiros 10^6 bytes
+  MPI_Recv(receive_buffer,max_buffr_size,MPI_PACKED,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+  //desempacota dimensao do pacote completo
+  MPI_Unpack(receive_buffer,max_buffr_size,&size_position,&f_size,1,MPI_INT,MPI_COMM_WORLD);
+  f_buffr = new(char[f_size]);
+  f_sender_tid = status.MPI_SOURCE;
+  f_msg_id = status.MPI_TAG;
+  f_position=sizeof(int);
+  //recebe demais blocos de 10^6 bytes
+  int receive_position;
+  for(receive_position=0;receive_position<max_buffr_size;receive_position++) 
+    if(receive_position<f_size) f_buffr[receive_position] = receive_buffer[receive_position];
+  for(int i=max_buffr_size;i<f_size;i = i+max_buffr_size){
+    MPI_Recv(receive_buffer,max_buffr_size,MPI_PACKED,f_sender_tid,f_msg_id,MPI_COMM_WORLD,&status);
+    for(int j=0;j<max_buffr_size;j++,receive_position++) 
+      if(receive_position<f_size) f_buffr[receive_position] = receive_buffer[receive_position];
+  }
+  return 1;
+}
+
+  // Metodos para DESEMPACOTAR dados do buffer.
+  //  p : Ponteiro para o buffer onde os dados serao lidos.
+  //  n : Numero de elementos a serem lidos (default: um unico dado).
+int OOPMPIReceiveStorage::UpkByte(char *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_CHAR,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(char);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkInt(int *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_INT,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(int);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkShort(short *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_SHORT,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(short);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkLong(long *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_LONG,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(long);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkUint(u_int *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_UNSIGNED,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(u_int);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkUshort(u_short *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_UNSIGNED_SHORT,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(u_short);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkUlong(u_long *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_UNSIGNED_LONG,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(u_long);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkFloat(float *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_FLOAT,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(float);
+  return 1;
+}
+
+int OOPMPIReceiveStorage::UpkDouble(double *p, int n){
+  MPI_Unpack(f_buffr,f_size,&f_position,p,n,MPI_DOUBLE,MPI_COMM_WORLD);
+  //f_position=f_position+n*sizeof(double);
+  return 1;
+}
+
+//essa eh meio complicada...
+int OOPMPIReceiveStorage::UpkStr(char *p){
+  int n=-1;
+  do{
+    n++;
+    MPI_Unpack(f_buffr,f_size,&f_position,&p[n],1,MPI_DOUBLE,MPI_COMM_WORLD);
+  }while(p[n] !='\0"');
+  //f_position=f_position+n;
+  return 0;
 }
 
 

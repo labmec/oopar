0277c77ec23b746121a91c1e923e9070a5952ff7
longhin <longhin@a721bac0-29f8-11de-897e-ef2ff8275b85>
2004-01-16 19:35:57 +0000

    Still going

--- trunk/Projects/BiCGSTAB/TUpdateResidue.h
+++ trunk/Projects/BiCGSTAB/TUpdateResidue.h
@@ -5,24 +5,23 @@
 #include <ooptask.h>
 class OOPMReturnType;
 class TUpdateResidue : public OOPTask {
-public:    
-
-    /**
-    * Returns the estimated execution time.
-    * returns 0 if the task is instantaneous
-    * returns > 0 if estimate is known
-    * return < 0 if no estimate is known
-    */
-	virtual long ExecTime ();
+public:
+  /**
+   * Returns the estimated execution time.
+   * returns 0 if the task is instantaneous
+   * returns > 0 if estimate is known
+   * return < 0 if no estimate is known
+   */
+  virtual long ExecTime ();
 
 	/**
-	* Execute the task, verifying that all needed data acesses are satisfied.
-	*/
+	 * Execute the task, verifying that all needed data acesses are satisfied.
+	 */
 	virtual OOPMReturnType Execute ();
 
 	/**
-	* Returns last created Id.
-	*/
+	 * Returns last created Id.
+	 */
 	virtual long GetClassID ()
 	{
 		return TDISTNORM_ID;

94db09bcd74233e1a97da4e9a8b34ac9cdf7ac9c
longhin <longhin@a721bac0-29f8-11de-897e-ef2ff8275b85>
2004-02-11 19:55:06 +0000

    New classes for the BiCG implementation

---
+++ trunk/Projects/BiCGSt/bicgdouble.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+                          bicgdouble.cpp  -  description
+                             -------------------
+    begin                : Wed Feb 11 2004
+    copyright            : (C) 2004 by longhin
+    email                : longhin@carlsberg.fec.unicamp.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "bicgdouble.h"
+
+BiCGDouble::BiCGDouble(){
+}
+BiCGDouble::~BiCGDouble(){
+}
+int BiCGDouble::Pack (OOPSendStorage * buf)
+{
+  OOPSaveable::Pack(buf);
+	long class_id = GetClassID ();
+	buf->PkLong (&class_id);
+  buf->PkDouble(&value);
+	return 1;
+}
+/**************/
+/*** Unpack ***/
+int BiCGDouble::Unpack (OOPReceiveStorage * buf)
+{
+  OOPSaveable::Unpack(buf);
+  double aux;
+  buf->UpkDouble(&aux);
+  value=aux;  
+	return 1;
+}
+long BiCGDouble::GetClassID ()
+{
+	return BICGDOUBLE_ID;
+}

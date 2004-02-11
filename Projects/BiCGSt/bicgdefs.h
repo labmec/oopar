fb09b926796b95f02ff97f1102a3bd96cc670b05
longhin <longhin@a721bac0-29f8-11de-897e-ef2ff8275b85>
2004-02-11 19:55:06 +0000

    New classes for the BiCG implementation

---
+++ Projects/BiCGSt/bicgdefs.h
@@ -0,0 +1,20 @@
+/***************************************************************************
+                          bicgdefs.h  -  description
+                             -------------------
+    begin                : Wed Feb 11 2004
+    copyright            : (C) 2004 by longhin
+    email                : longhin@carlsberg.fec.unicamp.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#define TPARCOMPUTE_ID  4000
+#define BICGDOUBLE      4001
+#define TTASKCOMM_ID    4003
+#define TPARVECTOR_ID   4006
---
+++ Projects/BiCGSt/bicgdouble.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+                          bicgdouble.h  -  description
+                             -------------------
+    begin                : Wed Feb 11 2004
+    copyright            : (C) 2004 by longhin
+    email                : longhin@carlsberg.fec.unicamp.br
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef BICGDOUBLE_H
+#define BICGDOUBLE_H
+
+#include <bicgdefs.h>
+#include <oopsaveable.h>
+
+/**Implements the transmitable double variable
+  *@author longhin
+  */
+
+class BiCGDouble : public OOPSaveable {
+public:
+  double value;
+	BiCGDouble();
+	~BiCGDouble();
+  long GetClassID():
+
+  int Pack (OOPSendStorage * buf);
+	int Unpack (OOPReceiveStorage * buf);
+  
+};
+
+#endif

73c0c5a7ac227a1415452dccb236486b52c7e09c
nobody <nobody@a721bac0-29f8-11de-897e-ef2ff8275b85>
2004-05-26 15:13:55 +0000

    This commit was manufactured by cvs2svn to create tag 'Before_Remove_OOPStorage'.

--- Projects/Flux/pzvecflux.cpp
+++
@@ -1,6 +0,0 @@
-#include "pzvec.h"
-#include "TContribution.h"
-//#ifdef PZENVIRONMENT
-template class TPZVec < TPZVec >;
-template class TPZVec < TContribution >;
-template class TPZVec < TContribution * >;
--- Sources/cmdefs.h
+++
@@ -1,5 +0,0 @@
-//
-// Identificadores das classes que podem ser transmitidas pelo
-//  Communication Manager.
-//
-#define CID_TSAVEABLE -1

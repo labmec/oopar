5d5a362474be24f151391b48d0691dba80bfd156
longhin <longhin@a721bac0-29f8-11de-897e-ef2ff8275b85>
2006-10-06 20:55:45 +0000

    main com 4 processos

--- Projects/TestTask/main.cpp
+++ Projects/TestTask/main.cpp
@@ -15,6 +15,10 @@
 
 #include "OOPDataLogger.h"
 
+#ifdef OOP_MPE
+#include "oopevtmanager.h"
+#endif
+
 
 #include "TTaskTest.h"
 #include "OOPInt.h"
@@ -39,7 +43,7 @@ OOPDataManager *DM;
 OOPTaskManager *TM;
 
 
-int main (int argc, char **argv)
+int mainorig (int argc, char **argv)
 {
 	CM = new OOPMPICommManager (argc, argv);
 	char filename[256];
@@ -89,7 +93,7 @@ int main (int argc, char **argv)
 	
 	
 		cout << "Task A " << tta->Submit() << endl;
-		cout << "Task B " << ttb->Submit() << endl;
+ 		cout << "Task B " << ttb->Submit() << endl;
 		
 		
 		OOPWaitTask * wt = new OOPWaitTask(0);
@@ -97,6 +101,105 @@ int main (int argc, char **argv)
 				IdA, EWriteAccess, ver));
 		wt->AddDependentData(OOPMDataDepend(
 				IdB, EWriteAccess, ver));
+		cout << "Wait Task " << wt->Submit()  << endl;
+		cout << "Calling Wait Task\n";
+		wt->Wait();
+		wt->Finish();
+		cout << "Wait task finished\n";
+		int iproc;
+		for(iproc=1; iproc<CM->NumProcessors(); iproc++)
+		{
+			OOPTerminationTask *task = new OOPTerminationTask(iproc);
+			task->Submit();
+		}
+		sleep(1);
+		OOPTerminationTask *task = new OOPTerminationTask(0);
+		task->Submit();
+	}
+
+	
+	TM->Wait();
+	delete  DM;
+	delete  TM;
+	delete  CM;
+	delete LogDM;
+
+	cout << "Leaving mpimain\n";
+	cout.flush();
+	return 0;
+	
+}
+int main (int argc, char **argv)
+{
+	CM = new OOPMPICommManager (argc, argv);
+
+#ifdef OOP_MPE
+        gEvtDB.AddStateEvent("taskexec","Task Execution",CM->GetProcID()==0);
+        gEvtDB.AddSoloEvent("grantaccess","Grant Access",CM->GetProcID()==0);
+
+#endif
+	char filename[256];
+	sprintf(filename,"datalogger%d", CM->GetProcID());
+	OOPDataLogger * LogDM = new OOPDataLogger(filename);
+	::LogDM = LogDM;
+	sprintf(filename,"tasklog.log%d", CM->GetProcID());
+	TaskLog.open(filename);
+	sprintf(filename,"datalog.log%d", CM->GetProcID());
+	DataLog.open(filename);
+	sprintf(filename,"datamanlog.log%d", CM->GetProcID());
+	DataManLog.open(filename);
+	sprintf(filename,"transferdata.log%d", CM->GetProcID());
+	TransferDataLog.open(filename);
+	sprintf(filename,"taskqueue.log%d", CM->GetProcID());
+	TaskQueueLog.open(filename);
+	sprintf(filename,"taskmanlog.log%d", CM->GetProcID());
+	TaskManLog.open(filename);
+	sprintf(filename,"dataqueuelog.log%d", CM->GetProcID());
+	DataQueueLog.open(filename);
+
+	TM = new OOPTaskManager (CM->GetProcID ());
+	DM = new OOPDataManager (CM->GetProcID ());
+
+		
+	TM->Execute();
+	if(!CM->GetProcID()){
+		OOPInt * inta = new OOPInt;
+		OOPInt * intb = new OOPInt;
+		OOPObjectId IdA, IdB;
+		IdA = DM->SubmitObject(inta, 1);
+//		IdB = DM->SubmitObject(intb, 1);
+
+		TTaskTest * tta = new TTaskTest(0);
+		TTaskTest * ttb = new TTaskTest(1);
+		TTaskTest * ttc = new TTaskTest(2);
+		TTaskTest * ttd = new TTaskTest(3);
+		OOPDataVersion ver;
+		
+		ttb->AddDependentData(OOPMDataDepend(
+				IdA, EWriteAccess, ver));
+/*		ttb->AddDependentData(OOPMDataDepend(
+				IdB, EWriteAccess, ver));			*/
+                ++ver;
+		tta->AddDependentData(OOPMDataDepend(
+				IdA, EWriteAccess, ver));
+		++ver;			
+		ttc->AddDependentData(OOPMDataDepend(
+				IdA, EWriteAccess, ver));			
+		++ver;			
+		ttd->AddDependentData(OOPMDataDepend(
+				IdA, EWriteAccess, ver));			
+	
+		cout << "Task A " << tta->Submit() << endl;
+ 		cout << "Task B " << ttb->Submit() << endl;
+ 		cout << "Task C " << ttc->Submit() << endl;
+ 		cout << "Task D " << ttd->Submit() << endl;
+		
+		++ver;
+		OOPWaitTask * wt = new OOPWaitTask(0);
+		wt->AddDependentData(OOPMDataDepend(
+				IdA, EReadAccess, ver));
+/*		wt->AddDependentData(OOPMDataDepend(
+				IdB, EWriteAccess, ver));*/
 		cout << "Wait Task " << wt->Submit()  << endl;
 		cout << "Calling Wait Task\n";
 		wt->Wait();

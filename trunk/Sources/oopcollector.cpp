7ccc6727531d5271e08a66272d1bbb5f5ec1b3b4
gustavolonghin <gustavolonghin@a721bac0-29f8-11de-897e-ef2ff8275b85>
2009-04-28 16:56:30 +0000

    Introduced OOPCollector class. Responsible for Gather and Scatter operations.

---
+++ trunk/Sources/oopcollector.cpp
@@ -0,0 +1,11 @@
+/*
+ *  oopcollector.cpp
+ *  OOPar
+ *
+ *  Created by Gustavo Longhin on 4/28/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "oopcollector.h"
+

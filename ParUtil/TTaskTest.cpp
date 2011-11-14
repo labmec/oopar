aefc0b61744d6960993718e66bc737458e5aeb5e
jorge.diazcalle@gmail.com <jorge.diazcalle@gmail.com@a721bac0-29f8-11de-897e-ef2ff8275b85>
2011-11-14 21:32:29 +0000

    Created files for doxygen documentation Was created a new group of auxiliar files (ParUtil)

---
+++ ParUtil/TTaskTest.cpp
@@ -0,0 +1,50 @@
+#include "TTaskTest.h"
+#include "OOPInt.h"
+#include "ooptaskmanager.h"
+#include "oopterminationtask.h"
+
+
+OOPMReturnType TTaskTest::Execute()
+{
+	/*
+	 int wait = 0;
+	 wait = 1;
+	 cout << "Executing Task " << Id() << " Goint out in " << wait << " secs\n";
+	 OOPInt * lint = dynamic_cast<OOPInt*>(fDependRequest.ObjectPtr( 0));
+	 lint->fValue = lint->fValue + 1000;
+	 cout << "Value of Int " << lint->fValue << endl;;
+	 cout.flush();
+	 //  sleep(wait);
+	 //  IncrementWriteDependentData();
+	 */
+	cout << "Trigering Termination Tasks\n " << endl;
+	cout.flush();
+	int i = 0;
+	for(i = 0; i < 4; i++)
+	{
+		OOPTerminationTask * tt = new OOPTerminationTask(i);
+		TM()->Submit(tt);
+	}
+	return ESuccess;
+}
+
+void TTaskTest::Write(TPZStream & buf, int withclassid){
+	OOPTask::Write(buf, withclassid);
+	m_Matrix.Write(buf,0);
+	m_Mesh.Write(buf,0);
+	int clsid = ClassId();
+	buf.Write(&clsid);
+}
+
+void TTaskTest::Read(TPZStream & buf, void * context){
+	OOPTask::Read(buf, context);
+	m_Matrix.Read(buf,0);
+	m_Mesh.Read(buf,0);
+	int clsid = 0;
+	buf.Read(&clsid);
+	if(ClassId()!=clsid){
+		cout << "ClassId Missmatch\n";
+	}
+}
+
+template class TPZRestoreClass<TTaskTest, TTASKTEST_ID>;
---
+++ ParUtil/TTaskTest.h
@@ -0,0 +1,57 @@
+
+#ifndef TTASKTEST_H
+#define TTASKTEST_H
+#include "ooptask.h"
+#include "pzsave.h"
+#include "pzfmatrix.h"
+#include "pzgengrid.h"
+#include "pzgmesh.h"
+
+#define TTASKTEST_ID 1000
+
+class   TTaskTest :public OOPTask
+{
+	TPZFMatrix m_Matrix;
+	TPZGeoMesh m_Mesh;
+public:
+	virtual ~TTaskTest(){}
+	
+	TTaskTest(){} 
+	/**
+     * @brief Simple constructor  
+     */
+	TTaskTest (int ProcId) : OOPTask(ProcId) , m_Matrix(5000,5000,1.) {
+		TPZVec<int> nx(2,1000);
+		TPZVec<REAL> x0(3,0);
+		TPZVec<REAL> x1(3,1);
+		TPZGenGrid grid(nx, x0, x1,1,0);
+		grid.Read(m_Mesh);
+	} 
+    /**
+	 * @brief Execute the task, verifying that all needed data acesses are satisfied. \n
+	 * The TParCompute is OOPTask descendent class and this method provides the access by the TaskManager.
+	 * @since 02/06/2003
+	 */
+	virtual OOPMReturnType Execute (); 
+	
+	virtual int ClassId () const
+	{
+		return TTASKTEST_ID; 
+	}
+	/**
+	 * @brief Packs the object in on the buffer so it can be transmitted through the network. \n
+	 * The Pack function  packs the object's class_id while function Unpack() doesn't,
+	 * allowing the user to identify the next object to be unpacked.
+	 * @param buf A pointer to TSendStorage class to be packed.
+	 */
+	void Write (TPZStream & buf,int withclassid);
+	/**
+	 * @brief Unpacks the object class_id
+	 * @param buf A pointer to TSendStorage class to be unpacked.
+	 */
+	void Read (TPZStream & buf, void * context);
+};
+
+extern ofstream TaskLog;
+
+#endif // TLOCALCOMPUTE_H

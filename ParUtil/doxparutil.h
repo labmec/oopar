e5a40b68a7ecd7913109633ac9247ab3203578dc
jorge.diazcalle@gmail.com <jorge.diazcalle@gmail.com@a721bac0-29f8-11de-897e-ef2ff8275b85>
2011-11-15 15:02:20 +0000

    Improved organization of the testing socket files

--- ParUtil/doxparutil.h
+++ ParUtil/doxparutil.h
@@ -7,3 +7,8 @@
  * @defgroup parutil The auxiliar classes.
  * @brief This module contains all classes which defines auxiliar objects to communication.
  */
+
+/**
+ * @defgroup socket The classes to communication using socket
+ * @brief This module contains all classes which defines communications using socket.
+ */

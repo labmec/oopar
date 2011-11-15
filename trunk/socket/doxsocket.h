556ca3305cf741d25011e06c7167a81bf3feb6b5
jorge.diazcalle@gmail.com <jorge.diazcalle@gmail.com@a721bac0-29f8-11de-897e-ef2ff8275b85>
2011-11-15 14:57:55 +0000

    This directory was moved into Project directory as TestSocket

--- trunk/socket/doxsocket.h
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- * @brief Creates socket group for Doxygen documentation.
- */
-
-/**
- * @defgroup socket The classes to communication using socket
- * @brief This module contains all classes which defines communications using socket.
- */

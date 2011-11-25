2b0268776502b689fc79dd01d566ea6f880e8fb7
jorge.diazcalle@gmail.com <jorge.diazcalle@gmail.com@a721bac0-29f8-11de-897e-ef2ff8275b85>
2011-11-25 12:03:58 +0000

    Improved comments for doxygen and method implementation to compile

--- trunk/Projects/TestTask/TTaskTest.h
+++ trunk/Projects/TestTask/TTaskTest.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * @brief Contains the definition of the TTaskTest class to TestTask project.
+ */
 #ifndef TTASKTEST_H
 #define TTASKTEST_H
 #include "ooptask.h"
@@ -6,20 +10,21 @@
 #include "pzgengrid.h"
 #include "pzgmesh.h"
 
-
-
 #define TTASKTEST_ID 1000
 
 class   TTaskTest :public OOPTask
 {
-   TPZFMatrix m_Matrix;
-   TPZGeoMesh m_Mesh;
-public:
-	virtual ~TTaskTest(){}
+	TPZFMatrix m_Matrix;
+	TPZGeoMesh m_Mesh;
 	
+public:
+	/** @brief Default destructor */
+	virtual ~TTaskTest(){}
+	/** @brief Default constructor */
 	TTaskTest(){} 
+	
 	/**
-     * Simple constructor  
+     * @brief Simple constructor given the id of the processor
      */
 	TTaskTest (int ProcId) : OOPTask(ProcId) , m_Matrix(5000,5000,1.) {
 		TPZVec<int> nx(2,1000);
@@ -27,31 +32,36 @@ public:
 		TPZVec<REAL> x1(3,1);
 		TPZGenGrid grid(nx, x0, x1,1,0);
 		grid.Read(m_Mesh);
-	} 
+	}
+	
     /**
-	* Execute the task, verifying that all needed data acesses are satisfied.
-	* The TParCompute is OOPTask descendent class and this method provides the access by the TaskManager.
-	* @since 02/06/2003
-	*/
+	 * @brief Execute the task, verifying that all needed data acesses are satisfied. \n
+	 * The TParCompute is OOPTask descendent class and this method provides the access by the TaskManager.
+	 * @since 02/06/2003
+	 */
 	virtual OOPMReturnType Execute (); 
-
+	
 	virtual int ClassId () const
 	{
 		return TTASKTEST_ID; 
 	}
-  /**
-   * Packs the object in on the buffer so it can be transmitted through the network.
-   * The Pack function  packs the object's class_id while function Unpack() doesn't,
-   * allowing the user to identify the next object to be unpacked.
-   * @param *buff A pointer to TSendStorage class to be packed.
-   */
-	void Write (TPZStream & buf,int withclassid);
-  /**
-   * Unpacks the object class_id
-   * @param *buff A pointer to TSendStorage class to be unpacked.
-   */
-	void Read (TPZStream & buf, void * context);
+	
+	/**
+	 * @brief Packs the object in on the buffer so it can be transmitted through the network. \n
+	 * The Pack function  packs the object's class_id while function Unpack() doesn't,
+	 * allowing the user to identify the next object to be unpacked.
+	 * @param buf A pointer to TPZStream class to be packed.
+	 * @param withclassid identifier of the current class
+	 */
+	void Write(TPZStream & buf,int withclassid);
+	/**
+	 * @brief Unpacks the object class_id
+	 * @param buf A pointer to TPZStream class to be unpacked.
+	 * @param context Pointer of the data.
+	 */
+	void Read(TPZStream & buf, void * context);
 };
 
 extern ofstream TaskLog;
+
 #endif // TLOCALCOMPUTE_H
